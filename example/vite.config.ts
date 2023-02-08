3fc1b62cbf9a5bf51b2c44df464f981f795b2d79
Mikkel RINGAUD <mikkel@milescode.dev>
2023-02-08 22:08:09 +0000

    chore: add base for vite config
    since github pages serves on a subpath, we have to tell vite we are serving on a subpath

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import solid from 'vite-plugin-solid';
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [solid()],
 
   server: {
     port: 3000,
   },
+
+  /** GitHub Pages deploy sub-path. */
+  base: "/solid-boring-avatars/",
 
   build: {
     target: 'esnext',
