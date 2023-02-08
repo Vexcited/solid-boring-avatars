956a31302bb69ec4ac3acb17432b495d1739f47a
Mikkel RINGAUD <mikkel@milescode.dev>
2023-02-08 22:43:03 +0000

    feat(example): add bauhaus

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,17 @@
 import type { Component } from 'solid-js';
 import Avatar from "solid-boring-avatars";
 
+import { createSignal } from "solid-js";
+
 const App: Component = () => {
+ 
+
   return (
     <div class="flex flex-col items-center justify-center gap-4 min-h-screen">
       <h1 class="mb-4 font-medium text-xl">SolidJS, boring-avatars</h1>
 
       <Avatar variant="beam" />
+      <Avatar variant="bauhaus" />
     </div>
   );
 };
